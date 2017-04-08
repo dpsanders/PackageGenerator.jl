0a093217e4a446182afd4db3ef147bcd7baa4579
autodocs <autodocs>
2017-04-08 08:45:21 +0000

    build based on f6bdd89

---
+++ latest/search_index.js
@@ -0,0 +1,27 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#PackageGenerator.test_function-Tuple{}",
+    "page": "Home",
+    "title": "PackageGenerator.test_function",
+    "category": "Method",
+    "text": "test_function()\n\nReturn 1\n\njulia> import PackageGenerator\n\njulia> PackageGenerator.test_function()\n1\n\n\n\n"
+},
+
+{
+    "location": "index.html#PackageGenerator.jl-1",
+    "page": "Home",
+    "title": "PackageGenerator.jl",
+    "category": "section",
+    "text": "Documentation for PackageGenerator.jlModules = [PackageGenerator]"
+},
+
+]}
