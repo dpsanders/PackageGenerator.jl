24e61b683a4cfc37fcb30d3d576224ee052d3ea1
Brandon Taylor <brandon.taylor221@gmail.com>
2017-05-15 22:21:53 -0400

    added files

---
+++ test/local_tests.jl
@@ -0,0 +1,17 @@
+using PackageGenerator
+
+user = read(PackageGenerator.User)
+configure(user.github_token, user.appveyor_token)
+update_configuration(ssh_keygen_file = "C:/Program Files/Git/usr/bin/ssh-keygen")
+
+package = "NewTestPackage9" |> Package |> generate
+
+# should error
+package |> PackageGenerator.AppVeyor |> PackageGenerator.check
+
+fake_travis = package |> PackageGenerator.Travis
+fake_travis.repo_name = "fake"
+# should error reasonably
+PackageGenerator.set_repo_code!(fake_travis)
+
+PackageGenerator.delete(package)
