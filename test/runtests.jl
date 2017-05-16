98dcd71b7cb5d9084535d105930b1f30a870f064
Brandon Taylor <brandon.taylor221@gmail.com>
2017-05-15 22:35:19 -0400

    added some more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,8 +15,16 @@ using Base.Test
 
 configure("", "", travis_token = "")
 update_configuration(sync_time = 50)
+
 package = "test_package" |> Package
 @test package.sync_time == 50
+@test_throws ErrorException Package("blah.jl")
+
+@test PackageGenerator.GitHub(package).url == "https://api.github.com"
+@test PackageGenerator.Travis(package).url == "https://api.travis-ci.org"
+@test PackageGenerator.Travis().url == "https://api.travis-ci.org"
+@test PackageGenerator.AppVeyor(package).url == "https://ci.appveyor.com/"
+
 path = package.path
 mkpath(package.path)
 PackageGenerator.write_texts(package)
@@ -38,12 +46,10 @@ texts = (
     ".codecov",
     ".gitignore",
 )
-
 @test all(texts) do file
     joinpath(path, file) |> ispath
 end
 
-@test_throws ErrorException Package("blah.jl")
 @test_throws ErrorException PackageGenerator.ssh_keygen("blah")
 
 rm(path, recursive = true)
